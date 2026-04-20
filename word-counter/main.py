d3c8aca08bdff6e51929c9726b33041ed6ca9074
Nirmal raj G <106583548+Nirmalraj-G@users.noreply.github.com>
2026-04-20 11:52:31 +0530

    Implement word counting functionality
    Add a word counter that ignores common words.

--- word-counter/main.py
+++ word-counter/main.py
@@ -1,1 +1,22 @@
+a = input("Type the sentence: ").split()
+word_count = { }
+
+ignore  = ['the', 'is', 'and', 'a', 'an', 'was']
+
+for word in a:
+    word = word.lower()
+    
+    clean = ""
+    
+    for ch in word:
+        if ch.isalpha():
+            clean += ch
+            
+    if clean:
+        if clean in word_count:
+            word_count[clean] += 1
+        else:
+            word_count[clean] = 1
+    
+print(word_count)
 
